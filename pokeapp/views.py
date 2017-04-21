5269d09e742bf357c4d742158c8d759daa223b34
Erox <erox@localhost.localdomain>
2017-04-21 19:47:46 +0200

    login/logout

--- pokeapp/views.py
+++ pokeapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse, Http404
+from django.template import Context
+from django.template.loader import get_template
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def home(request):
+    template = get_template('home.html')
+    variables = Context({
+        'Title' : 'Pokeapp',
+        'user' : request.user
+    })
+    page = template.render(variables)
+
+    return HttpResponse(page)
--- pokemons/urls.py
+++ pokemons/urls.py
@@ -14,7 +14,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.contrib.auth.views import login, logout
+from pokeapp.views import home
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', login, name='login'),
+    url(r'^accounts/logout/$', logout, name='logout'),
+    url(r'^home/$', home),
 ]
